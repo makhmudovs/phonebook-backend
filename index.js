5b022b9430bfc21086efcd7403fecf5cbe41bce5
makhmudovs <makhmudovs50@gmail.com>
2025-02-04 21:40:28 +0800

    phonebook-backend:morgan installed

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.post("/api/persons", (req, res) => {
   res.json(newPerson);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT: ", PORT);
