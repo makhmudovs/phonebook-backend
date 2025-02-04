b8be10cbb7819f193ffc7b078d5e46e536b2f1ff
makhmudovs <makhmudovs50@gmail.com>
2025-02-04 21:35:46 +0800

    phonebook-backend:endpoints and array changed

--- index.js
+++ index.js
@@ -1,88 +1,113 @@
 const express = require("express");
-const cors = require("cors");
+const morgan = require('morgan');
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-let notes = [
+app.use(express.json());
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',JSON.stringify(req.body)
+  ].join(' ')
+}));
+let persons = [
   {
     id: "1",
-    content: "HTML is easy",
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
 
-const requestLogger = (req, res, next) => {
-  console.log("Method: ", req.method);
-  console.log("Path: ", req.path);
-  console.log("Body: ", req.body);
-  console.log("---");
-  next();
-};
+app.get("/api/persons", (req, res) => {
+  res.send(persons);
+});
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
+app.get("/api/info", (req, res) => {
+  res.send(`
+    <div>
+        <p>Phone book has info for ${persons.length} people</p>
+        <p>${new Date.toLocaleString()}</p>
+    </div>`);
+});
 
-app.post("/api/notes", (req, res) => {
-  const body = req.body;
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
 
-  if (!body.content) {
-    return res.status(400).json({
-      error: "content missing",
+  if (person) {
+    res.json(person);
+  } else {
+    return res.status(404).json({
+      error: `Person with this id: ${id} not found`,
     });
   }
-
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  };
-
-  notes = notes.concat(note);
-  response.json(note);
 });
 
-app.get("/api/notes/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-  const id = req.params.id;
-  notes = notes.filter((note) => note.id !== id);
+  persons = persons.filter((person) => person.id !== id);
 
   res.status(204).end();
 });
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+const generateUniqueId = () => {
+  let newId;
+  let isUnique = false;
+
+  while (!isUnique) {
+    newId = Math.floor(Math.random() * 1000000).toString();
+    isUnique = !persons.some((entry) => entry.id === newId);
+  }
+
+  return newId;
+};
+
+app.post("/api/persons", (req, res) => {
+  const { name, number } = req.body;
+  if (!name || !number || name.trim() === "" || number.trim() === "") {
+    return res.status(400).json({
+      error: "Missing information",
+    });
+  }
+
+  const foundName =
+    persons.find((p) => p.name.toLowerCase() === name.toLowerCase()) ||
+    persons.find((p) => p.name.toLowerCase().includes(name.toLowerCase()));
+  const foundNubmer = persons.find((p) => p.number === number);
+  if (foundName || foundNubmer) {
+    return res.status(400).json({
+      error: "Name or number already exists",
+    });
+  }
+
+  const newPerson = {
+    name,
+    number,
+    id: generateUniqueId(),
+  };
+  persons = persons.concat(newPerson);
+  res.json(newPerson);
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
-
-app.use(requestLogger);
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server is running on PORT: ", PORT);
 });
